d727ad371bd27cafe2ec341e9b4dd4753d7dbb60
Diego Silva <112778845+DiegoSilvaCBA@users.noreply.github.com>
2023-12-06 20:13:53 -0300

    Update controllers

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,20 +1,70 @@
-const Reserva = require('../models/Reserva');
+const Usuario = require('../models/Usuario');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
-const criarReserva = async (req, res) => {
+const registro = async (req, res) => {
   try {
-    const reserva = new Reserva(req.body);
-    await reserva.save();
-    res.status(201).json(reserva);
+    const { nome, email, senha, perfil } = req.body;
+
+    // Verifica se o e-mail já está em uso
+    const usuarioExistente = await Usuario.findOne({ email });
+    if (usuarioExistente) {
+      return res.status(400).json({ error: 'E-mail já cadastrado' });
+    }
+
+    // Criptografar a senha antes de salvar no banco de dados
+    const senhaCriptografada = await bcrypt.hash(senha, 10);
+
+    const usuario = new Usuario({
+      nome,
+      email,
+      senha: senhaCriptografada,
+      perfil,
+    });
+
+    await usuario.save();
+    res.status(201).json({ message: 'Usuário registrado com sucesso' });
   } catch (error) {
     console.error(error);
-    res.status(400).json({ error: 'Não foi possível criar a reserva' });
+    res.status(500).json({ error: 'Erro interno do servidor' });
   }
 };
 
-const listarReservas = async (req, res) => {
+const login = async (req, res) => {
   try {
-    const reservas = await Reserva.find();
-    res.json(reservas);
+    const { email, senha } = req.body;
+
+    // Verifica se o usuário existe
+    const usuario = await Usuario.findOne({ email });
+    if (!usuario) {
+      return res.status(401).json({ error: 'Credenciais inválidas' });
+    }
+
+    // Verifica a senha
+    const senhaCorreta = await bcrypt.compare(senha, usuario.senha);
+    if (!senhaCorreta) {
+      return res.status(401).json({ error: 'Credenciais inválidas' });
+    }
+
+    // Gere um token de autenticação
+    const token = jwt.sign({ id: usuario._id, perfil: usuario.perfil }, 'chave_secreta_do_jwt');
+
+    res.json({ token });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Erro interno do servidor' });
+  }
+};
+
+const listarUsuarios = async (req, res) => {
+  try {
+    // Verifica se o usuário autenticado é um administrador
+    if (req.usuario.perfil !== 'admin') {
+      return res.status(403).json({ error: 'Acesso negado' });
+    }
+
+    const usuarios = await Usuario.find();
+    res.json(usuarios);
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Erro interno do servidor' });
@@ -22,6 +72,7 @@ const listarReservas = async (req, res) => {
 };
 
 module.exports = {
-  criarReserva,
-  listarReservas,
+  registro,
+  login,
+  listarUsuarios,
 };
